890c0ead2319a2b5730d487003c46d7a32cbd227
tiendung-nguyent <nguyenthitiendung@gmail.com>
2026-03-27 19:46:09 +0700

    Dung update models.py class SanPham

--- Nhom1_ChuDe1/app/models.py
+++ Nhom1_ChuDe1/app/models.py
@@ -67,15 +67,24 @@ class DanhMuc(models.Model):
         return self.DM_Ten
 
 
-
 class SanPham(models.Model):
+    TRANG_THAI_CHOICES = [
+        (0, 'Đang bán'),
+        (1, 'Hết hàng'),
+        (2, 'Ngừng bán'),
+    ]
+
     SP_Ma = models.CharField(max_length=10, primary_key=True)
     DM_Ma = models.ForeignKey(DanhMuc, on_delete=models.CASCADE, related_name='san_pham_thuoc_dm')
     SP_Ten = models.CharField(max_length=100)
-    SP_GiaBan = models.DecimalField(max_digits=10, decimal_places=2)
+    SP_GiaBan = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     SP_MoTa = models.TextField(null=True, blank=True)
     SP_URLHinhAnh = models.URLField(max_length=500, null=True, blank=True)
-    SP_TrangThai = models.CharField(max_length=50)
+
+    SP_TrangThai = models.IntegerField(
+        choices=TRANG_THAI_CHOICES,
+        default=0
+    )
 
     def __str__(self):
         return self.SP_Ten
