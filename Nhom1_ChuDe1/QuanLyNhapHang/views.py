3b49dd96b91de3f3ed4cd5ab835c840be2d48992
tiendung-nguyent <nguyenthitiendung@gmail.com>
2026-04-13 01:23:52 +0700

    update cau truc thu muc

--- Nhom1_ChuDe1/QuanLyNhapHang/views.py
+++ Nhom1_ChuDe1/QuanLyNhapHang/views.py
@@ -1,3 +1,3 @@
 from django.shortcuts import render
 def nhap_hang_view(request):
-    return render(request, 'staff/QuanLyNhapHang/QuanLyNhapHang.html')
+    return render(request, 'QuanLyNhapHang/QuanLyNhapHang.html')
--- Nhom1_ChuDe1/gioHang/views.py
+++ Nhom1_ChuDe1/gioHang/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 def gio_hang(request):
-    return render(request, 'user/gioHang/gio_hang.html')
+    return render(request, 'gioHang/gio_hang.html')
 def thanh_toan_view(request):
-    return render(request, 'user/gioHang/ThanhToan.html')
+    return render(request, 'gioHang/ThanhToan.html')
 
 def trangChuUser(request):
-    return render(request, 'user/gioHang/trangChuUser.html')
+    return render(request, 'gioHang/trangChuUser.html')
 
 def chiTietSanPham(request):
-    return render(request, 'user/gioHang/chiTietSanPham.html')
+    return render(request, 'gioHang/chiTietSanPham.html')
