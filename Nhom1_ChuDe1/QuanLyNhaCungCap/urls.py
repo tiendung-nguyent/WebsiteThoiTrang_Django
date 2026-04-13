3ef34ac180f9cb473b1d02b9fc4f0ebfa255b144
ThanhHanh16 <thanhhanh0104@gmail.com>
2026-04-13 13:20:36 +0700

    cap nhat

--- Nhom1_ChuDe1/QuanLyNhaCungCap/urls.py
+++ Nhom1_ChuDe1/QuanLyNhaCungCap/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('staff/nha-cung-cap/', views.quan_ly_ncc_view, name='quan_ly_ncc'),
+    path('staff/nha-cung-cap/them/', views.them_ncc_view, name='them_ncc'),
+    path('staff/nha-cung-cap/sua/<str:ma_ncc>/', views.sua_ncc_view, name='sua_ncc'),
+    path('staff/nha-cung-cap/xoa/<str:ma_ncc>/', views.xoa_ncc_view, name='xoa_ncc'),
 ]
