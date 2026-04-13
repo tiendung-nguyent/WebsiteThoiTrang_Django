38d8726f9948be87aac29667cd2a49f0399e7df4
tiendung-nguyent <nguyenthitiendung@gmail.com>
2026-04-13 23:56:27 +0700

    datDat

--- Nhom1_ChuDe1/quanLyDonHang/urls.py
+++ Nhom1_ChuDe1/quanLyDonHang/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('staff/quanLyDonHang/', views.quanLyDonHang, name='quanLyDonHang'),
-    path('staff/quanLyDonHang/view/<str:status>/', views.view_quanLyDonHang, name='view_quanLyDonHang'),
+    path('staff/quanLyDonHang/view/<str:order_id>/', views.view_quanLyDonHang, name='view_quanLyDonHang'),
 ]
